9d7629d011f5b9757cedc110253130148a462eba
Mark Polyakov <mark@markasoftware.com>
2016-03-30 17:19:56 -0700

    styling, big box, vh units, etc

--- src/public/scripts/section-controller.js
+++ src/public/scripts/section-controller.js
@@ -3,6 +3,6 @@ var selectionImgs = document.querySelectorAll('#selector-icon-container > img');
 
 [].forEach.call(selectionImgs, function(curImg, curIndex){
     curImg.addEventListener('click', function(){
-        slider.style.marginLeft = 'calc(4.6em * ' + curIndex + ' - 2px)';
+        slider.style.marginLeft = 'calc(9vh * ' + curIndex + ' - 2px)';
     });
 });
